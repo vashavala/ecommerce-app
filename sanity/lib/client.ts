803b46030080149494a56d2640d6782d51436cfc
vasha <vashavala@outlook.com>
2023-08-01 21:50:14 +0800

    hide data; add env variable

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,5 +7,5 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
-  token: "skq1ghgT9oEUznAZD7Tcy0JkzEuE3742TMwec2cRzCY1ELPmSGuqXQqwVeGFIu6IpJfyarndCKGpkm938f5CbiSeS1dh3jT8eRFWyA1F0JUYE8kJgj5t9FGGb6eyPgXX60cqlXTbOCzQa43JU9r5e4mXynsZJUCfOO0GES8XyyPhvOmXanT3"
+  // token: "skq1ghgT9oEUznAZD7Tcy0JkzEuE3742TMwec2cRzCY1ELPmSGuqXQqwVeGFIu6IpJfyarndCKGpkm938f5CbiSeS1dh3jT8eRFWyA1F0JUYE8kJgj5t9FGGb6eyPgXX60cqlXTbOCzQa43JU9r5e4mXynsZJUCfOO0GES8XyyPhvOmXanT3"
 })
